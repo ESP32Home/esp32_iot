035f67d91672ffae9c9a44b945c2ae3f2ced912f
wassfila <wassim.filali@gmail.com>
2020-10-08 19:50:23 +0200

    moved static folder

--- docsource/source/conf.py
+++ docsource/source/conf.py
@@ -21,9 +21,6 @@ project = 'ESP32 IoT'
 copyright = '2020, wassfila'
 author = 'wassfila'
 
-# The full version, including alpha/beta/rc tags
-release = '1.0.0'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -31,7 +28,6 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,7 +36,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,9 +44,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
--- docsource/source/index.rst
+++ docsource/source/index.rst
@@ -1,6 +1,7 @@
 .. ESP32 IoT documentation master file, created by
-   sphinx-quickstart on Tue Oct  6 19:13:20 2020.
-   adapted file for test
+   sphinx-quickstart on Thu Oct  8 18:35:06 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
 
 Welcome to ESP32 IoT's documentation!
 =====================================
